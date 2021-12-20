d40c4a63fcbc8d0c75b241ea6acc6530fde64afa
KR <rosolino@pop-os.localdomain>
2021-12-20 20:32:01 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ['Source Sans Pro', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ types/environment.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+  interface ProcessEnv extends NodeJS.ProcessEnv {
+    DISCORD_CLIENT_ID: string
+    DISCORD_SECRET_ID: string
+  }
+}
