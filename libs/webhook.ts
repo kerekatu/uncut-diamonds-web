04f6d58e0dea66ef06f0e5d87b152e542133f485
KR <grplayeer@gmail.com>
2022-01-07 15:48:42 +0100

    fix: downgrade discord.js to 12.5.3

--- libs/webhook.ts
+++ libs/webhook.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed, WebhookClient } from 'discord.js'
+import { WebhookClient } from 'discord.js'
 import { IShopItem } from 'types/types'
 
 const { WEBHOOK_ID, WEBHOOK_TOKEN } = process.env
@@ -7,13 +7,10 @@ if (!WEBHOOK_ID || !WEBHOOK_TOKEN) {
   throw new Error('Missing environment variables')
 }
 
-const webhookClient = new WebhookClient({
-  id: WEBHOOK_ID,
-  token: WEBHOOK_TOKEN,
-})
+const webhookClient = new WebhookClient(WEBHOOK_ID, WEBHOOK_TOKEN)
 
 export default function logPurchase(id: string, item: IShopItem): void {
-  webhookClient.send({
-    content: `<@${id}> wydał/a pieniążki na: **${item.title}** <:pepepog:913174353208815686>`,
-  })
+  webhookClient.send(
+    `<@${id}> wydał/a pieniążki na: **${item.title}** <:pepepog:913174353208815686>`
+  )
 }
